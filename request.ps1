31d5a052e6ed7075d1bcd5750d6b217ce94772f7
Hans Starkopf <hansh.starkopf@gmail.com>
2023-08-08 00:01:42 +0300

    many updates

---
+++ request.ps1
@@ -0,0 +1,2 @@
+Invoke-WebRequest -Uri "http://127.0.0.1:8000/subscriptions" -Method POST -Body @{name="le guin";email="ursula_le_guin@gmail.com"}
+
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,27 @@
-fn main() {
-    println!("Hello, worlddd!");
+use std::net::TcpListener;
 
-    println!("Hello, worlddd!");
+use secrecy::ExposeSecret;
+use sqlx::PgPool;
+
+use zero2prod::configuration;
+use zero2prod::startup;
+use zero2prod::telemetry;
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let subscriber = telemetry::get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
+    telemetry::init_subscriber(subscriber);
+    // Panic if we can't read configuration
+    let configuration = configuration::get_configuration().expect("Failed to read configuration.");
+    let address = format!(
+        "{}:{}",
+        configuration.application.host, configuration.application.port
+    );
+    let listener = TcpListener::bind(address)?;
+    let connection_pool =
+        PgPool::connect_lazy(&configuration.database.connection_string().expose_secret())
+            // .await
+            .expect("Failed to connect to Postgres.");
+
+    startup::run(listener, connection_pool)?.await
 }
